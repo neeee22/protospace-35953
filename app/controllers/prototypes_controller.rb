d26c9e2a2522556cdb3802c990b1d5626aca0fbd
ninisanni <nee.kenji@gmail.com>
2021-06-18 15:48:17 +0900

    プロトタイプが表示されるようにする

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,12 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.includes(:user)
   end
+
   def new
     @prototype = Prototype.new
   end
+
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
@@ -12,6 +15,7 @@ class PrototypesController < ApplicationController
       render :new
     end
   end
+  
  private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
