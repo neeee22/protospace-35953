a2d2f6a7ffb4132cd3a9e0dae55f9d507ee16619
ninisanni <nee.kenji@gmail.com>
2021-06-18 12:33:24 +0900

    バリデーション設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
   def index
   end
-
+ # private
+ # def prototype_params
+ #   params.require(:)
 end
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,3 +1,10 @@
 class Prototype < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :cath_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+  
 end
