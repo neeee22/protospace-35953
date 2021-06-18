f707dbb0cbf508285d035f231849ba452435ce35
ninisanni <nee.kenji@gmail.com>
2021-06-18 18:09:56 +0900

    編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes, only: [:new,:create,:show]
+  resources :prototypes, only: [:new,:create,:show,:edit,:update]
   devise_scope :user do
     get "users", to: "devise/registrations#new"
   end
