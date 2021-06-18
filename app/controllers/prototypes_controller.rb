ae2ee0667231307631050af6ab90ce0cfacd5d06
ninisanni <nee.kenji@gmail.com>
2021-06-19 02:27:32 +0900

    ユーザーのアクセス制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new,:edit,:destroy]
+  before_action :move_to_index, only: [:edit]
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -42,5 +44,12 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    prototype = Prototype.find(params[:id])
+    unless current_user.id == prototype.user.id
+      redirect_to action: :index
+    end
   end
 end
